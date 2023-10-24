c91b48b2757370c4334a219da1f89cb6ae343375
AdminKong <stechno.muhsapa@gmail.com>
2023-10-24 20:23:42 +0700

    first commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className='text-9xl pt-10 text-blue-400'>
+        Test Navbar
+      </h1>
+    </div>
+  )
+}
+
+export default Navbar
